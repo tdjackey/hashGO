c7aea106474e67394b0f1a55ec280088aff69f0d
tdjackey <tdjackey@gmail.com>
2015-06-01 00:28:58 +0800

    done sm3

--- hw1/sm3/main.cpp
+++ hw1/sm3/main.cpp
@@ -14,7 +14,7 @@ long SM3_GG(long X,long Y,long Z,int j) {return ((j < 16) ? (X ^ Y ^ Z) : ((X & 
 long SM3_P0(long X) {return (X ^ (SM3_ROTL(X, 9)) ^ (SM3_ROTL(X, 17)));}
 long SM3_P1(long X) {return (X ^ (SM3_ROTL(X, 15)) ^ (SM3_ROTL(X, 23)));}
 
-char* SM3(const char *str, char *sm3, long long length, FILE *file=NULL, bool space=false){
+char* SM3(const char *str, char *sm3, long long length,  bool space=false){
 	char *pp,*ppend;
 	long l, i, j, T[64], W[68], W1[64], A, B, C, D, E, F, G, H, SS1, SS2, TT1, TT2;
 	long IV[8] = {0x7380166f,0x4914b2b9,0x172442d7,0xda8a0600,0xa96f30bc,0x163138aa,0xe38dee4d ,0xb0fb0e4e};
@@ -37,7 +37,7 @@ char* SM3(const char *str, char *sm3, long long length, FILE *file=NULL, bool sp
 			TT1 = (SM3_FF(A, B, C, j) + D + SS2 + W1[j]) & 0xFFFFFFFF;
 			TT2 = (SM3_GG(E, F, G, j) + H + SS1 + W[j]) & 0xFFFFFFFF;
 			D = C, C = SM3_ROTL(B, 9), B = A, A = TT1, H = G, G = SM3_ROTL(F, 19), F = E, E = SM3_P0(TT2);
-			if(file)fprintf(file, "%08X %08X %08X %08X %08X %08X %08X %08X\n", A, B, C, D, E, F, G, H); 
+			//printf("%08X %08X %08X %08X %08X %08X %08X %08X\n", A, B, C, D, E, F, G, H); 
 		}
 		IV[0] ^= A, IV[1] ^= B, IV[2] ^= C, IV[3] ^= D, IV[4] ^= E, IV[5] ^= F, IV[6] ^= G, IV[7] ^= H;
     }
@@ -49,20 +49,69 @@ char* SM3(const char *str, char *sm3, long long length, FILE *file=NULL, bool sp
 
 
 int main(){
-	char *str1="";
-	char *str2="abc";
-	char *str3="gongjieP14200004";
-	char *str4="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-	char *str5=new char[1000001];
+
+	
+	char *result=new char[700];
+
+
+	char *str1 = "";
+	cout << "*********************************************************************************************" << endl;
+	cout << "test1: NULL " << endl;
+	cout << "*********************************************************************************************" << endl;
+	cout << "中间结果：" << endl;
+	SM3(str1,result,0,1);
+	cout << "最终结果: " << endl;
+	cout << result << endl;
+	cout << endl;
+	
+
+	char *str2 = "abc";
+	cout << "*********************************************************************************************" << endl;
+	cout << "test2:  abc " << endl;
+	cout << "*********************************************************************************************" << endl;
+	cout << "中间结果：" << endl;
+	SM3(str2,result,3,1);
+	cout << "最终结果: " << endl;
+	cout << result << endl;
+	cout << endl;
+
+
+	char *str3 = "gongjieP14200004";
+	cout << "*********************************************************************************************" << endl;
+	cout << "test3:  gongjieP14200004 " << endl;
+	cout << "*********************************************************************************************" << endl;
+	cout << "中间结果：" << endl;
+	SM3(str3,result,16,1);
+	cout << "最终结果: " << endl;
+	cout << result << endl;
+	cout << endl;
+
+
+	char *str4 = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+	cout << "*********************************************************************************************" << endl;
+	cout << "test4:  ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789 " << endl;
+	cout << "*********************************************************************************************" << endl;
+	cout << "中间结果：" << endl;
+	SM3(str4,result,62,1);
+	cout << "最终结果: " << endl;
+	cout << result << endl;
+	cout << endl;
+
+
+	char *str5 = new char[1000001];
 	for(int i=0;i<1000000;i++){
 		str5[i]='a';
 	}
 	str5[1000000]=0;
-
-	char *ans=new char[1000];
-	char* (*f[1])(const char* str, char* sha1, long long length, FILE* file, bool space)={SM3};
-	char fname[1][10]={"SM3"};
-	FILE* file=fopen("out.txt","w");
+	cout << "*********************************************************************************************" << endl;
+	cout << "test5:  一百万个a " << endl;
+	cout << "*********************************************************************************************" << endl;
+	cout << "中间结果：" << endl;
+	SM3(str5,result,1000000,1);
+	cout << "最终结果: " << endl;
+	cout << result << endl;
+	cout << endl;
+	/*
 	for(int i=0;i<1;i++){
 		fprintf(file,"%s:\n",fname[i]);
 		fprintf(file,"实例1: %s\n",str1);
@@ -82,5 +131,6 @@ int main(){
 		fprintf(file,"\n");
 	}
 	fclose(file);
-	system("pause");
+	system("pause");*/
+	return 0;
 }
