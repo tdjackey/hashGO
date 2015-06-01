075105e186d4eb8bcd80f42551f7528fc902056b
tdjackey <tdjackey@gmail.com>
2015-06-01 21:06:42 +0800

    worked

--- hw1/sha256/main.cpp
+++ hw1/sha256/main.cpp
@@ -16,14 +16,12 @@
 #define SHA256_E1(x) (SHA256_ROTL(x,26)^SHA256_ROTL(x,21)^SHA256_ROTL(x,7))
 #define SHA256_O0(x) (SHA256_ROTL(x,25)^SHA256_ROTL(x,14)^SHA256_SR(x,3))
 #define SHA256_O1(x) (SHA256_ROTL(x,15)^SHA256_ROTL(x,13)^SHA256_SR(x,10))
-char* SHA256(const char* str, char* sha256, long long length, FILE* file=NULL, bool space=false){
+char* SHA256(const char* str, char* sha256, unsigned int length, FILE* file=NULL, bool space=false){
     char *pp, *ppend;
-    long long l, i, W[64], T1, T2;
-    unsigned int A, B, C, D, E, F, G, H;
-    unsigned int H0, H1, H2, H3, H4, H5, H6, H7;
+    unsigned int  l, i, W[64], T1, T2, A, B, C, D, E, F, G, H, H0, H1, H2, H3, H4, H5, H6, H7;
     H0 = 0x6a09e667, H1 = 0xbb67ae85, H2 = 0x3c6ef372, H3 = 0xa54ff53a;
     H4 = 0x510e527f, H5 = 0x9b05688c, H6 = 0x1f83d9ab, H7 = 0x5be0cd19;
-    long long K[64] = {
+    unsigned int  K[64] = {
         0x428a2f98, 0x71374491, 0xb5c0fbcf, 0xe9b5dba5, 0x3956c25b, 0x59f111f1, 0x923f82a4, 0xab1c5ed5,
         0xd807aa98, 0x12835b01, 0x243185be, 0x550c7dc3, 0x72be5d74, 0x80deb1fe, 0x9bdc06a7, 0xc19bf174,
         0xe49b69c1, 0xefbe4786, 0x0fc19dc6, 0x240ca1cc, 0x2de92c6f, 0x4a7484aa, 0x5cb0a9dc, 0x76f988da,
@@ -34,13 +32,13 @@ char* SHA256(const char* str, char* sha256, long long length, FILE* file=NULL, b
         0x748f82ee, 0x78a5636f, 0x84c87814, 0x8cc70208, 0x90befffa, 0xa4506ceb, 0xbef9a3f7, 0xc67178f2,
     };
     l = length + ((length % 64 > 56) ? (128 - length % 64) : (64 - length % 64));
-    if (!(pp = (char*)malloc((unsigned long)l))) return 0;
+    if (!(pp = (char*)malloc((unsigned int )l))) return 0;
     for (i = 0; i < length; pp[i + 3 - 2 * (i % 4)] = str[i], i++);
     for (pp[i + 3 - 2 * (i % 4)] = 128, i++; i < l; pp[i + 3 - 2 * (i % 4)] = 0, i++);
-    *((long*)(pp + l - 4)) = length << 3;
-    *((long*)(pp + l - 8)) = length >> 29;
+    *((unsigned int  *)(pp + l - 4)) = length << 3;
+    *((unsigned int  *)(pp + l - 8)) = length >> 29;
     for (ppend = pp + l; pp < ppend; pp += 64){
-        for (i = 0; i < 16; W[i] = ((long*)pp)[i], i++);
+        for (i = 0; i < 16; W[i] = ((unsigned int *)pp)[i], i++);
         for (i = 16; i < 64; W[i] = (SHA256_O1(W[i - 2]) + W[i - 7] + SHA256_O0(W[i - 15]) + W[i - 16]), i++);
         A = H0, B = H1, C = H2, D = H3, E = H4, F = H5, G = H6, H = H7;
         for (i = 0; i < 64; i++){
--- hw2/main.cpp
+++ hw2/main.cpp
@@ -18,12 +18,12 @@ using namespace std;
 #define SHA256_O1(x) (SHA256_ROTL(x,15)^SHA256_ROTL(x,13)^SHA256_SR(x,10))
 
 
-char* SHA256(const char* str, char* sha256, long long length, bool space=false){
+char* SHA256(const char* str, char* sha256, unsigned int length,  bool space=false){
     char *pp, *ppend;
-    long l, i, W[64], T1, T2, A, B, C, D, E, F, G, H, H0, H1, H2, H3, H4, H5, H6, H7;
+    unsigned int  l, i, W[64], T1, T2, A, B, C, D, E, F, G, H, H0, H1, H2, H3, H4, H5, H6, H7;
     H0 = 0x6a09e667, H1 = 0xbb67ae85, H2 = 0x3c6ef372, H3 = 0xa54ff53a;
     H4 = 0x510e527f, H5 = 0x9b05688c, H6 = 0x1f83d9ab, H7 = 0x5be0cd19;
-    long K[64] = {
+    unsigned int  K[64] = {
         0x428a2f98, 0x71374491, 0xb5c0fbcf, 0xe9b5dba5, 0x3956c25b, 0x59f111f1, 0x923f82a4, 0xab1c5ed5,
         0xd807aa98, 0x12835b01, 0x243185be, 0x550c7dc3, 0x72be5d74, 0x80deb1fe, 0x9bdc06a7, 0xc19bf174,
         0xe49b69c1, 0xefbe4786, 0x0fc19dc6, 0x240ca1cc, 0x2de92c6f, 0x4a7484aa, 0x5cb0a9dc, 0x76f988da,
@@ -34,28 +34,29 @@ char* SHA256(const char* str, char* sha256, long long length, bool space=false){
         0x748f82ee, 0x78a5636f, 0x84c87814, 0x8cc70208, 0x90befffa, 0xa4506ceb, 0xbef9a3f7, 0xc67178f2,
     };
     l = length + ((length % 64 > 56) ? (128 - length % 64) : (64 - length % 64));
-    if (!(pp = (char*)malloc((unsigned long)l))) return 0;
+    if (!(pp = (char*)malloc((unsigned int )l))) return 0;
     for (i = 0; i < length; pp[i + 3 - 2 * (i % 4)] = str[i], i++);
     for (pp[i + 3 - 2 * (i % 4)] = 128, i++; i < l; pp[i + 3 - 2 * (i % 4)] = 0, i++);
-    *((long*)(pp + l - 4)) = length << 3;
-    *((long*)(pp + l - 8)) = length >> 29;
+    *((unsigned int  *)(pp + l - 4)) = length << 3;
+    *((unsigned int  *)(pp + l - 8)) = length >> 29;
     for (ppend = pp + l; pp < ppend; pp += 64){
-        for (i = 0; i < 16; W[i] = ((long*)pp)[i], i++);
+        for (i = 0; i < 16; W[i] = ((unsigned int *)pp)[i], i++);
         for (i = 16; i < 64; W[i] = (SHA256_O1(W[i - 2]) + W[i - 7] + SHA256_O0(W[i - 15]) + W[i - 16]), i++);
         A = H0, B = H1, C = H2, D = H3, E = H4, F = H5, G = H6, H = H7;
         for (i = 0; i < 64; i++){
             T1 = H + SHA256_E1(E) + SHA256_Ch(E, F, G) + K[i] + W[i];
             T2 = SHA256_E0(A) + SHA256_Maj(A, B, C);
             H = G, G = F, F = E, E = D + T1, D = C, C = B, B = A, A = T1 + T2;
-			//if(file)fprintf(file, "%08X %08X %08X %08X %08X %08X %08X %08X\n", A, B, C, D, E, F, G, H); 
+            //if(file)fprintf(file, "%08X %08X %08X %08X %08X %08X %08X %08X\n", A, B, C, D, E, F, G, H); 
         }
         H0 += A, H1 += B, H2 += C, H3 += D, H4 += E, H5 += F, H6 += G, H7 += H;
     }
     free(pp - l);
     if(space) sprintf(sha256, "%08X %08X %08X %08X %08X %08X %08X %08X", H0, H1, H2, H3, H4, H5, H6, H7);
-	else sprintf(sha256, "%08X%08X%08X%08X%08X%08X%08X%08X", H0, H1, H2, H3, H4, H5, H6, H7);
+    else sprintf(sha256, "%08X%08X%08X%08X%08X%08X%08X%08X", H0, H1, H2, H3, H4, H5, H6, H7);
     return sha256;
 }
+
 
 bool compare60(char *p1,char *p2){
 	for(int i=0;i<15;i++){
@@ -65,7 +66,7 @@ bool compare60(char *p1,char *p2){
 }
 
 int main(){
-	char *start="zzzzzzzzzzzzzzz";
+	char *start="aaaaaaaaaaaaaaa";
 	char *p1=new char[1000],*p2=new char[1000],*t1=new char[1000],*t2=new char[1000];
 	
 	strcpy(p1,start);
